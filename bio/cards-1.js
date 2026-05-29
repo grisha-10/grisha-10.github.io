b17112360fb58539042e378977685eae804838e5
webiGrisha <goramarcenko@gmail.com>
2026-05-29 19:06:46 +0300

    Add files via upload

---
+++ bio/cards-1.js
@@ -0,0 +1,61 @@
+const card10 = document.querySelector('.my-card-10');
+
+card10.addEventListener('click', () => {
+  card10.classList.toggle('rotated');
+});
+
+// =-------------------------------------
+const card11 = document.querySelector('.my-card-11');
+
+card11.addEventListener('click', () => {
+  card11.classList.toggle('rotated');
+});
+
+// =-------------------------------------
+const card12 = document.querySelector('.my-card-12');
+
+card12.addEventListener('click', () => {
+  card12.classList.toggle('rotated');
+});
+
+// =-------------------------------------
+const card13 = document.querySelector('.my-card-13');
+
+card13.addEventListener('click', () => {
+  card13.classList.toggle('rotated');
+});
+
+// =-------------------------------------
+const card14 = document.querySelector('.my-card-14');
+
+card14.addEventListener('click', () => {
+  card14.classList.toggle('rotated');
+});
+
+// =-------------------------------------
+const card15 = document.querySelector('.my-card-15');
+
+card15.addEventListener('click', () => {
+  card15.classList.toggle('rotated');
+});
+
+// =-------------------------------------
+const card16 = document.querySelector('.my-card-16');
+
+card16.addEventListener('click', () => {
+  card16.classList.toggle('rotated');
+});
+
+// =-------------------------------------
+const card17 = document.querySelector('.my-card-17');
+
+card17.addEventListener('click', () => {
+  card17.classList.toggle('rotated');
+});
+
+// =-------------------------------------
+const card18 = document.querySelector('.my-card-18');
+
+card18.addEventListener('click', () => {
+  card18.classList.toggle('rotated');
+});
